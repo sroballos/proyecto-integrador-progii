36b1f1f8d8e79e741163ed41f6019b7f1f0517bd
sroballos <sroballos@udesa.edu.ar>
2024-04-10 16:12:13 -0300

    Header y Footer Partials, Router

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-let profileController = require("../controllers/usersController");
+let usersController = require("../controllers/usersController");
 
 
-router.get('/', profileController.general);
-router.get('/edit', profileController.edit);
-router.get('/register', profileController.general);
-router.get('/login', profileController.general);
+router.get('/', usersController.general);
+router.get('/edit', usersController.edit);
+router.get('/register', usersController.general);
+router.get('/login', usersController.general);
 
 
 
