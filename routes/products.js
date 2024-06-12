2371d8a5888746f99d54ba5aa889d11d0a8590af
sroballos <sroballos@udesa.edu.ar>
2024-06-12 18:30:19 -0300

    Comentarios, Inicio de edición

--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,11 @@ var router = express.Router();
 let productController = require("../controllers/productController");
 
 
-router.get('/:id?', productController.general);
 router.get('/add', productController.add);
+router.get('/edit/:id', productController.edit);
 router.post("/agregar_producto", productController.store)
-router.get('/searchresult', productController.searchresults);
+router.get('/searchresult', productController.searchresults)
+router.get('/:id?', productController.general);
 
 
 module.exports = router;
