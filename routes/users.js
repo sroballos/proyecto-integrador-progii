4edeae65512446df7d034cecdb692f9e7e32a444
Tomás <tomiricci@MacBook-Pro-de-Tomi-2.local>
2024-06-12 11:02:10 -0300

    Corrigiendo el register y el login

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,31 @@ const {body} = require("express-validator");
 const db = require("../database/models");
 
 const validaciones = [
+  body("username")
+    .notEmpty().withMessage("Debes completar este campo").bail()
+    .isAlpha().withMessage("Tu usuario debe contener solo letras"),
   body("email")
     .notEmpty().withMessage("Debes completar este campo").bail()
-    .isEmail().withMessage("Escribí un mail válido"),
+    .isEmail().withMessage("Escribí un mail válido").bail()
+    .custom(function(value, { req }){
+      return db.User.findOne({
+        where: { email: req.body.email },
+      })
+          .then(function(user){
+             if(user){}
+          })
+     }).withMessage("Este mail ya está en uso, probá con otro"),
   body("password")
     .notEmpty().withMessage("Debes completar este campo").bail()
     .isLength({ min: 4 }).withMessage("Tu contraseña debe contener al menos 4 caracteres")
 ];
 
-router.get('/profile', usersController.general);
-router.get('/profile/edit', usersController.edit);
-router.get('/register', usersController.register);
+router.get('/', usersController.general);
+router.get('/edit', usersController.edit);
+router.get('/register', validaciones, usersController.register);
 router.post('/register', validaciones, usersController.register);
-router.get('/login', usersController.loginForm);
+router.get('/login', usersController.login);
 router.post('/login', validaciones, usersController.login);
-router.post('/logout', usersController.logout);
 
 
 module.exports = router;
